c5ef0fd2a5957ae6bd76a5adbbdf8e329216f248
Daniel Jardine <1005730@student.sae.edu.au>
2018-06-14 20:58:28 +1000

    Finished
    Added models and a sort of end. UI is buggy but workable.

---
+++ Assets/Scripts/ColourChange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourChange : MonoBehaviour {
+
+    public Color altColor = Color.red;
+    public Renderer rend;
+
+    // Use this for initialization
+    void Start () {
+        //Get the renderer of the object so we can access the color
+        rend = GetComponent<Renderer>();
+        //Set the initial color (0f,0f,0f,0f)
+       // rend.material.color = altColor;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "End")
+        {
+            //Alter the color          
+            altColor.g += 0.5f;
+            //Assign the changed color to the material.
+            rend.material.color = altColor;
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+
+    //Pause screen objects
+    GameObject[] pauseObjects;
+
+    // Use this for initialization
+    void Start () {
+        //When not paused play at normal speed
+        Time.timeScale = 1;
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+        hidePaused();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                showPaused();
+            }
+            else if (Time.timeScale == 0)
+            {
+                Time.timeScale = 1;
+                hidePaused();
+            }
+        }
+    }
+
+    public void pauseControl()
+    //Determines if the game is paused or not
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            showPaused();
+        }
+        else if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            hidePaused();
+        }
+    }
+
+    //shows objects with ShowOnPause tag
+    public void showPaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    //hides objects with ShowOnPause tag
+    public void hidePaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    //Scene Changes
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    //Quit Game, only when game is built
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
